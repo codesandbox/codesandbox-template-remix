6e8102d5947a56262d60d77722cfcbf941039226
Jasper De Moor <jasperdemoor@gmail.com>
2022-04-21 14:04:07 +0200

    Fix hmr port

--- remix.config.js
+++ remix.config.js
@@ -2,10 +2,11 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  ignoredRouteFiles: [".*"]
+  ignoredRouteFiles: [".*"],
+  // We need to keep the hmr and server port the same for the previews to work
+  devServerPort: +(process.env.PORT || "3000")
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
-  // devServerPort: 8002
 };
