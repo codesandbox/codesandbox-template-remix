1a1763361e531f57777844be91bb274e151e0d93
Michaël De Boey <info@michaeldeboey.be>
2022-10-31 12:48:55 +0100

    feat: bring in line with official template (#7)

--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,8 @@
-/**
- * @type {import('@remix-run/dev').AppConfig}
- */
+/** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
-  ignoredRouteFiles: [".*"]
+  ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
-  // devServerPort: 8002
 };
